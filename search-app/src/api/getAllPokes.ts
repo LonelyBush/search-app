dfa9fe8264d050d01a008d2a3c7466c0029c4d57
LonelyBush <lonely.bush68@gmail.com>
2024-07-08 18:41:13 +0300

    fix: fixed fetch url in getAllPokes

--- search-app/src/api/getAllPokes.ts
+++ search-app/src/api/getAllPokes.ts
@@ -1,7 +1,5 @@
 export async function getPokes(url: string, query: string = '') {
-  const response = await fetch(
-    `https://cors-anywhere.herokuapp.com/${url}${query}`,
-  );
+  const response = await fetch(`${url}${query}`);
   return response.json();
 }
 
