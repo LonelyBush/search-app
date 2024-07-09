0046aab216458228b6a3eb7d064bd589d21fa09c
LonelyBush <lonely.bush68@gmail.com>
2024-07-09 15:29:05 +0300

    fix: fixed error boundary and deleted console log

--- search-app/src/components/error_boundry/error_boundary.tsx
+++ search-app/src/components/error_boundry/error_boundary.tsx
@@ -1,4 +1,4 @@
-import { Component, ErrorInfo, ReactNode } from 'react';
+import { Component, ReactNode } from 'react';
 import './error_boundary-style.css';
 
 interface ErrorBoundaryProps {
@@ -16,21 +16,13 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   }
 
   static getDerivedStateFromError(): ErrorBoundaryState {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
-  }
-
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    // Log the error or send it to an error reporting service
-    console.error(error, errorInfo);
-    // Update state to show the fallback UI
   }
 
   render() {
     const { hasError } = this.state;
     const { children } = this.props;
     if (hasError) {
-      // You can render any custom fallback UI
       return (
         <div className="error-container">
           <h2>Something went wrong...</h2>
