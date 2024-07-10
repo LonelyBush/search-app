95a14500dda91e72cf88e5152dcf5185fc5f9e00
LonelyBush <lonely.bush68@gmail.com>
2024-07-10 18:21:04 +0300

    feat: convert search-page to the functional component

--- search-app/src/view/search-page.tsx
+++ search-app/src/view/search-page.tsx
@@ -1,4 +1,4 @@
-import { Component, FormEvent, PropsWithChildren } from 'react';
+import { FormEvent, useState } from 'react';
 import ItemsList from '../components/items_list/items_list';
 import './search-page-style.css';
 import { PokeSearchValue } from '../interfaces/api_interfaces';
@@ -8,39 +8,35 @@ interface State extends PokeSearchValue {}
 
 const searchValFromLS = localStorage.getItem('search-value');
 
-class SearchPage extends Component<PropsWithChildren, State> {
-  constructor(props: PropsWithChildren) {
-    super(props);
-    this.state = { searchValue: '', hasError: false };
-  }
+function SearchPage() {
+  const [searchPageState, setSearchPageState] = useState<State>({
+    searchValue: '',
+    hasError: false,
+  });
 
-  handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const searchValue = new FormData(e.currentTarget).get('search-input');
-    this.setState({
+    setSearchPageState({
       searchValue: `${searchValue}`,
+      hasError: false,
     });
   };
+  const { searchValue } = searchPageState;
 
-  render() {
-    const { searchValue, hasError } = this.state;
-    if (hasError) {
-      throw new Error('Oh no, you clicked error button!');
-    }
-    return (
-      <div className="main-content-section">
-        <h2>Poke Search</h2>
-        <SearchBar
-          searchValue={searchValFromLS}
-          handleSubmit={this.handleSubmit}
-          triggerError={() => {
-            this.setState({ hasError: true });
-          }}
-        />
-        <ItemsList searchValue={searchValue} />
-      </div>
-    );
-  }
+  return (
+    <div className="main-content-section">
+      <h2>Poke Search</h2>
+      <SearchBar
+        searchValue={searchValFromLS}
+        handleSubmit={handleSubmit}
+        triggerError={() => {
+          setSearchPageState({ searchValue: '', hasError: true });
+        }}
+      />
+      <ItemsList searchValue={searchValue} />
+    </div>
+  );
 }
 
 export default SearchPage;
